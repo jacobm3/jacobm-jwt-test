ea7fc83460565f528b7f0b6ca7690158582b427c
Jacob Martinson <jacobm3@gmail.com>
2023-04-21 00:42:09 -0500

    Update 00-mysql-setup.sh to work with RDS

--- vault-mysql-setup/00-mysql-setup.sh
+++ vault-mysql-setup/00-mysql-setup.sh
@@ -1,8 +1,33 @@
-#!/bin/bash 
+#!/bin/bash
 
-rootpass=root123
+# 2023.04.21 - updates to work with AWS RDS mysql, which doesn't allow SUPER privileges or granting ALL on *.* or mysql.*
 
-mysqlcmd="mysql -uroot -p${rootpass} -h localhost --protocol=TCP"
+if [[ -z "${MYSQL_HOST}" ]]; then
+  echo "ERROR: MYSQL_HOST environment variable is not set"
+  exit 1
+fi
+
+if [[ -z "${MYSQL_ROOT_USER}" ]]; then
+  echo "ERROR: MYSQL_ROOT_USER environment variable is not set"
+  exit 1
+fi
+
+if [[ -z "${MYSQL_ROOT_PASS}" ]]; then
+  echo "ERROR: MYSQL_ROOT_PASS environment variable is not set"
+  exit 1
+fi
+
+if [[ -z "${MYSQL_VAULT_PASS}" ]]; then
+  echo "ERROR: MYSQL_VAULT_PASS environment variable is not set"
+  exit 1
+fi
+set -e
+
+# set -x
+# --protocol=TCP required for localhost for some reason?
+# mysqlcmd="mysql -u ${MYSQL_ROOT_USER} -p ${MYSQL_ROOT_PASS} -h${MYSQL_HOST} --protocol=TCP"
+
+mysqlcmd="mysql --user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASS} --host=${MYSQL_HOST}"
 
 echo "Setting up vault's grant in mysql"
 # generate random mysql passwd for vault acct
@@ -10,14 +35,14 @@ echo "Setting up vault's grant in mysql"
 #echo $passwd > .mysql-vault-passwd
 
 
-$mysqlcmd <<EOF
+mysql --user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASS} --host=${MYSQL_HOST} << EOF
 DROP DATABASE IF EXISTS db1;
 CREATE DATABASE db1;
 
 use mysql;
 DROP USER IF EXISTS 'vault'@'%';
-CREATE USER 'vault'@'%' IDENTIFIED BY 'vaultpass';
-GRANT SUPER ON *.* to 'vault'@'%' WITH GRANT OPTION;
+CREATE USER 'vault'@'%' IDENTIFIED BY "${MYSQL_VAULT_PASS}";
+GRANT ALL PRIVILEGES ON db1.* to 'vault'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
@@ -25,29 +50,6 @@ echo "Creating db1 database"
 $mysqlcmd <<EOF
 create database if not exists db1;
 USE db1;
-CREATE TABLE IF NOT EXISTS tasks (
-    task_id INT AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    start_date DATE,
-    due_date DATE,
-    status TINYINT NOT NULL,
-    priority TINYINT NOT NULL,
-    description TEXT,
-    PRIMARY KEY (task_id)
-)  ENGINE=INNODB;
-CREATE TABLE IF NOT EXISTS users (
-    task_id INT AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    start_date DATE,
-    due_date DATE,
-    status TINYINT NOT NULL,
-    priority TINYINT NOT NULL,
-    description TEXT,
-    PRIMARY KEY (task_id)
-)  ENGINE=INNODB;
-
-create database if not exists db3;
-USE db3;
 CREATE TABLE IF NOT EXISTS tasks (
     task_id INT AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
@@ -77,10 +79,10 @@ vault secrets enable database
 echo "Writing db1 DB secrets engine config"
 vault write database/config/db1 \
     plugin_name=mysql-database-plugin \
-    connection_url="{{username}}:{{password}}@tcp(mysql-dev:3306)/" \
+    connection_url="{{username}}:{{password}}@tcp(${MYSQL_HOST}:3306)/" \
     allowed_roles="db1-5s,db1-30s" \
-    username="root" \
-    password="$rootpass"
+    username="${MYSQL_ROOT_USER}" \
+    password="${MYSQL_ROOT_PASS}"
 
 echo "Writing DB1 5s engine role" 
 vault write database/roles/db1-5s \
@@ -93,30 +95,6 @@ echo "Writing db1 policy"
 
 vault policy write db1 -<<EOF
 path "database/creds/db1-5s" {
-  capabilities = ["read"]
-}
-EOF
-
-
-echo "Writing db3 DB secrets engine config"
-vault write database/config/db3 \
-    plugin_name=mysql-database-plugin \
-    connection_url="{{username}}:{{password}}@tcp(mysql-dev:3306)/" \
-    allowed_roles="db3-5s,db3-30s" \
-    username="root" \
-    password="$rootpass"
-
-echo "Writing DB3 5s engine role" 
-vault write database/roles/db3-5s \
-    db_name=db3 \
-    creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON db3.* TO '{{name}}'@'%';" \
-    default_ttl="5s" \
-    max_ttl="5s"
-
-echo "Writing db3 policy"
-
-vault policy write db3 -<<EOF
-path "database/creds/db3-5s" {
   capabilities = ["read"]
 }
 EOF
